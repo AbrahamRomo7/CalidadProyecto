df39a8777cad21fdf2a979c0ea2bd7d995417f44
Davidm2606 <130103820+Davidm2606@users.noreply.github.com>
2025-02-04 14:12:14 -0500

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,14 @@
+public class Order {
+    private String customer;
+    private String orderDetail;
+
+    public Order(String customer, String orderDetail) {
+        this.customer = customer;
+        this.orderDetail = orderDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer: " + customer + ", Order: " + orderDetail;
+    }
+}
